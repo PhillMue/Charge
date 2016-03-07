ad873a80578a37640391f35b3abfbd55c19038fc
PhillMue <muenzuve@gmail.com>
2016-03-07 15:10:55 +0300

    Initial commit

---
+++ app/src/main/java/com/app/mue/charge/SplashScreen.java
@@ -0,0 +1,43 @@
+package com.app.mue.charge;
+
+/**
+ * Created by mue on 3/7/16.
+ */
+
+
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
